96ed65bdf65a7dd0f5bc98b66d37d55cbac060e9
sonakallacheril <76269703+sonakallacheril@users.noreply.github.com>
2020-12-30 19:38:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first console project")
+
+    var first_name = "Sona";
+    var last_name = "Kallacheril"
+
+    var full_name = first_name.concat(last_name);
+    console.log(full_name); 
+
+    var a = 10;
+    var b = 5;
+
+    var add = a + b;
+    console.log(add);
+
+    var subtract = a - b;
+    console.log(subtract);
+
+    var multiply = a * b;
+    console.log(multiply)
+
+    var divide = a / b;
+    console.log(divide)
